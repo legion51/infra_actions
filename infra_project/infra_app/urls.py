56e5c922cf8594929484ee989e4df645575eaf39
legion51 <57018749+legion51@users.noreply.github.com>
2023-05-14 14:09:14 +0300

    flake8

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -1,13 +1,11 @@
-from django.contrib import admin
-from django.urls import include, path
-from django.views.generic import TemplateView
+from django.urls import path
+
+from . import views
+
+app_name = 'infra_app'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path(
-        'redoc/',
-        TemplateView.as_view(template_name='redoc.html'),
-        name='redoc'
-    ),
+    path('', views.index, name='index'),
+    path('second_page/', views.second_page, name='second_page'),
+
 ]
